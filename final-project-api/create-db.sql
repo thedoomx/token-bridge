43fcbeb5e5dd1261d54593fd66b48285d4fd93bb
Stanislav Budinov <budinov.stanislav@gmail.com>
2023-03-19 21:44:59 +0200

    update db create schema script

--- final-project-api/create-db.sql
+++ final-project-api/create-db.sql
@@ -1,8 +1,12 @@
+CREATE DATABASE Erc20bridge;
+
 CREATE TABLE [Erc20bridge].public.events (
   ID SERIAL PRIMARY KEY,
   address_from VARCHAR(50),
   address_to VARCHAR(50),
 	amount INTEGER,
+	nonce INTEGER,
+  signed_message VARCHAR(65),
 	event_type VARCHAR(10),
     is_active BOOLEAN
 );
